81a78e32b6454cf040bb4abc7a75f3493fd3f305
Tarik <nikitin-taras@yandex.ru>
2023-07-16 23:04:29 +0200

    Create task_test

---
+++ Task_test.py
@@ -0,0 +1,18 @@
+# data = [1, 2, 3, 5, 8, 15, 23, 38]
+# res = list()
+
+# for i in data:
+#     if i % 2 == 0:
+#         res.append((i, i ** 2))
+# print(res)
+
+
+
+
+
+data = [1, 2, 3, 5, 8, 15, 23, 38]
+
+res = map(int, data)
+res = filter(lambda x: x % 2 == 0, res)
+res = list(map(lambda x: (x, x**2), res))
+print(res)
